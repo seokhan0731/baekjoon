59bedfebdc5a34546da5a65d20d27a833fa25a48
Seokhwan Kim <wistin@naver.com>
2026-01-15 13:42:06 +0900

    [Bronze II] Title: 블랙잭, Time: 104 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -4,6 +4,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+/**
+ * 초창기 풀이 알고리즘 구상
+ * 처음에는 그리디 알고리즘을 통해 풀고자 접근
+ * 1. m보다 작은 수 중, 가장 큰 수(a)를 선택
+ * 2. m-a보다 작은 수 중, 가장 큰 수(b)를 선택
+ * 3. m-a-b보다 작은 수 중, 가장 큰 수(c)를 선택
+ * <p>
+ * 하지만, a를 너무 큰 숫자로 뽑게 된다면, b+c 조합 중, 해당 조건을 만족하는 카드가 존재하지 않을 수 있기 때문에 부적합 판단
+ * => 이에 따라, 모든 경우의 수를 탐색하는 브루트포스 알고리즘 선택
+ *
+ * +) m보다 3개의 숫자 합이 작거나 같으면서 최댓값을 찾으면 되니, m>=sum 조건하에 max>sum은 기억할 필요 X
+ * => 추가적인 메모리 절약 가능
+ */
 public class Main {
     static int findMax(int[] arr, int goal) {
         int length = arr.length;
@@ -23,7 +36,7 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-        //System.setIn(new FileInputStream("input.txt"));
+//        System.setIn(new FileInputStream("input.txt"));
         StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
