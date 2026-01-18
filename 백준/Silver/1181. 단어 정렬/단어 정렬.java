b0def53733cc31fa71e01820d3e87b373acc6c3c
Seokhwan Kim <wistin@naver.com>
2026-01-18 22:56:09 +0900

    Enhance word sorting with custom comparator and comments
    Refactor sorting logic and add comments for clarity.

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -4,11 +4,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
+/**
+ * 1. 람다식을 통해 정렬 조건 커스텀
+ * 1-1. 메소드 시그니처: Arrays.sort(T[] a, Comparator<? super T> c)
+ *      정렬 대상(a)
+ *      람다식: (Comparator c) ->{...} 이 꼴로 작성
+ * 1-2.람다식 내부의 코드는 c언어의 strcmp()와 같은 부호에 따른 선후 관계
+ *          (-): 기존 순서 유지(제대로 되어있는 상태)
+ *          0: 서로 동일
+ *          (+): 자리 바꿈
+ *
+ * 2. 중복 제거 패턴
+ * 초기 버전에서는 n=1인 경우를 생각을 못해, i=0부터 i+1과 비교하여 i를 출력
+ * (?): n=1인 경우 별도 분기 처리 필요, 끝의 두 원소에 대한 별도 처리 필요
+ * => i=0 별도 출력 이후, i와 i-1 비교를 통한 i 출력 처리로 리팩토링
+ */
 public class Main {
 
-
     public static void main(String[] args) throws IOException {
-        //System.setIn(new FileInputStream("input.txt"));
+//        System.setIn(new FileInputStream("input.txt"));
         StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
