3edf43fce272b1ccaefbf07e35a2be9db8bf18b3
Seokhwan Kim <wistin@naver.com>
2026-01-21 14:14:28 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 28844 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -5,15 +5,30 @@ import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+/**
+ * [기존 코드] deque를 통한 queue 자료 구조 구현
+ * 기존 stack, queue 라이브러리 속 add, pop등을 사용하면 deque에 있는 first, last 개념과 헷갈림
+ * -> deque 라이브러리 속 메소드 사용 숙달 핊요
+ *
+ * -----------** deque **-----------
+ * 앞이 first, 뒤가 last라고 생각하면 된다.
+ * add/push -> offer()
+ * pop/remove -> poll()
+ * +) 앞/뒤 조회 -> peek()
+ *
+ * => 스택에서는 Last In First Out -> offerLast(), pollLast()
+ * => 큐에서는 First In First Out -> offerLast(), pollFirst()
+ * ---------------------------------
+ */
 class Main {
     static int popAndChange(Deque<Integer> queue) {
         while (true) {
             if (queue.size() == 1) {
-                return queue.getLast();
+                return queue.peekLast();
             }
-            queue.pop();
-            queue.add(queue.getFirst());
-            queue.pop();
+            queue.pollFirst();
+            queue.offerLast(queue.peekFirst());
+            queue.pollFirst();
         }
     }
 
@@ -26,7 +41,7 @@ class Main {
         Deque<Integer> queue = new ArrayDeque<>();
 
         for (int i = 0; i < n; i++) {
-            queue.add(i+1);
+            queue.offerLast(i+1);
         }
 
         System.out.println(popAndChange(queue));
