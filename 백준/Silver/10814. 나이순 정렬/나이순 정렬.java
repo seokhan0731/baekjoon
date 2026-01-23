c7d780e97a43528bf858f17c895e53628ab4113c
Seokhwan Kim <wistin@naver.com>
2026-01-23 13:27:04 +0900

    [Silver V] Title: 나이순 정렬, Time: 520 ms, Memory: 44996 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -5,6 +5,20 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/**
+ * 기존 코드
+ * 클래스의 배열을 통해, Arrays.sort 람다식을 활용한 정렬 수행
+ *
+ * 최적화를 한다면, 지금 코드에서는 signUpOrder가 필요가 없음
+ * Arrays.sort()-> 듀얼 퀵소트 or 팀소트
+ * -1. 듀얼 퀵소트: 자료형이 원시 자료형인 경우(int, float, char...)
+ * -2. 팀소트: 자료형이 객체인 경우(Integer, String, Class...)
+ *
+ * 듀얼 퀵소트는 unstable하기 때문에, 정렬 기준이 같더라도 순서가 바뀔 수 있지만,
+ * 팀소트는 stable하여 순서가 바뀌지 않는다.
+ * => 해당 문제에서는 나이가 같다면, 기존에 들어온 순서 기준으로 정렬 수행이기 때문에, 굳이 signUpOrder가 없어도
+ *    팀소트를 활용하여 정렬하였으면 답을 도출할 수 있었다.
+ */
 public class Main {
     static class Member {
         int age;
